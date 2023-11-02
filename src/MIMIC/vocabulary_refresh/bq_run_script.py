7b0abaa96fa09839684f99852713030e0e018785
Mike Shubov <38743245+shubov@users.noreply.github.com>
2023-11-02 23:17:48 +0000

    MacOS fix

--- src/MIMIC/vocabulary_refresh/bq_run_script.py
+++ src/MIMIC/vocabulary_refresh/bq_run_script.py
@@ -15,73 +15,76 @@ import datetime
 # ----------------------------------------------------
 
 config_default = {
-
     "variables": {
-
-        "@variable_1":      "No project replacement by default",
-        "@variable_2":      "No dataset replacement by default"
+        "@variable_1": "No project replacement by default",
+        "@variable_2": "No dataset replacement by default",
     },
-    
-    "escaping_chars": {
-        '"': '\\"'
-        # '`': '\\`' # don't replace in windows OS
-    }
+    "escaping_chars": {'"': '\\"', "`": "\\`"},  # don't replace in windows OS
 }
 
 # ----------------------------------------------------
 # read_params()
 # ----------------------------------------------------
 
-def read_params():
 
-    print('Reading params...')
+def read_params():
+    print("Reading params...")
     params = {
-        "etlconf_file":     "",
-        "config_file":      "",
-        "script_files":     [],
-        "files_not_found":  []
+        "etlconf_file": "",
+        "config_file": "",
+        "script_files": [],
+        "files_not_found": [],
     }
-    
+
     # Parsing command line arguments
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"e:c:",["etlconf=,config="])
+        opts, args = getopt.getopt(sys.argv[1:], "e:c:", ["etlconf=,config="])
         if len(args) == 0:
-            raise getopt.GetoptError("read_params() error", "Mandatory argument is missing.")
+            raise getopt.GetoptError(
+                "read_params() error", "Mandatory argument is missing."
+            )
 
     except getopt.GetoptError as err:
         print(err.args)
         print("Please indicate correct params:")
-        print("etlconf_file:   optional: indicate '-e' for 'etlconf', global config json file")
-        print("config_file:    optional: indicate '-c' for 'config', local config json file")
-        print("script_files:   [mandatory: indicate at least one script name as unnamed argument]")
+        print(
+            "etlconf_file:   optional: indicate '-e' for 'etlconf', global config json file"
+        )
+        print(
+            "config_file:    optional: indicate '-c' for 'config', local config json file"
+        )
+        print(
+            "script_files:   [mandatory: indicate at least one script name as unnamed argument]"
+        )
         sys.exit(2)
 
     # get config files namess
     for opt, arg in opts:
-        if opt == '-e' or opt == '--etlconf':
+        if opt == "-e" or opt == "--etlconf":
             if os.path.isfile(arg):
-                params['etlconf_file'] = arg
-        if opt == '-c' or opt == '--config':
+                params["etlconf_file"] = arg
+        if opt == "-c" or opt == "--config":
             if os.path.isfile(arg):
-                params['config_file'] = arg
+                params["config_file"] = arg
 
     # collect script names
     for arg in args:
         if os.path.isfile(arg):
-            params['script_files'].append(arg)
+            params["script_files"].append(arg)
         else:
-            params['files_not_found'].append(arg)
+            params["files_not_found"].append(arg)
 
-    print('scripts to run', params)
+    print("scripts to run", params)
     return params
+
 
 # ----------------------------------------------------
 # read_config()
 # ----------------------------------------------------
 
+
 def read_config(etlconf_file, config_file):
-    
-    print('Reading config...')
+    print("Reading config...")
     config = {}
     config_read = {}
     etlconf_read = {}
@@ -98,7 +101,7 @@ def read_config(etlconf_file, config_file):
     for k in config_default:
         s = etlconf_read.get(k, config_default[k])
         config[k] = s
-    
+
     # local config has higher priority
     for k in config_default:
         s = config_read.get(k, config[k])
@@ -107,9 +110,11 @@ def read_config(etlconf_file, config_file):
     print(config)
     return config
 
+
 # ----------------------------------------------------
 # trim_queries()
 # ----------------------------------------------------
+
 
 def trim_queries(s_queries):
     s_result = []
@@ -119,50 +124,52 @@ def trim_queries(s_queries):
             s_result.append(q)
     return s_result
 
+
 # ----------------------------------------------------
 # remove_comments()
 # ----------------------------------------------------
 
-def remove_comments(s_query):
 
-    print('Remove_comments()...')
+def remove_comments(s_query):
+    print("Remove_comments()...")
     # print(s_query)
 
-    s_lines_src = s_query.split('\n')
+    s_lines_src = s_query.split("\n")
     s_result = ""
 
     for s in s_lines_src:
-        comment_flag = s.replace(' ', '')[0:2]
+        comment_flag = s.replace(" ", "")[0:2]
 
-        if comment_flag != '--' and len(s) > 0:
-            s_result = s_result + s + '\n'
+        if comment_flag != "--" and len(s) > 0:
+            s_result = s_result + s + "\n"
 
     # print('Comments removed...')
     # print(s_result)
 
     return s_result
 
+
 # ----------------------------------------------------
 # format_query()
 # ----------------------------------------------------
 
-def format_query(s_query, config):
 
-    print('Formatting query...')
+def format_query(s_query, config):
+    print("Formatting query...")
 
     s_result = s_query
 
-    for var, val in config['escaping_chars'].items():
+    for var, val in config["escaping_chars"].items():
         s_result = s_result.replace(var, val)
 
-    for var, val in config['variables'].items():
+    for var, val in config["variables"].items():
         s_result = s_result.replace(var, val)
 
     print(s_result)
     return s_result
 
 
-'''
+"""
 ----------------------------------------------------
     1) make the command a single line
     windows or maybe 2022 style
@@ -171,28 +178,29 @@ def format_query(s_query, config):
 
     bqc: ready to run bq command 
 ----------------------------------------------------
-'''
+"""
+
+
 def troubleshooting_bqc_format(bqc):
+    print("Troubleshooting_bqc_format()...")
 
-    print('Troubleshooting_bqc_format()...')
-
-    s_lines_src = bqc.split('\n')
+    s_lines_src = bqc.split("\n")
     s_result = ""
 
     for s in s_lines_src:
-
         # remove trailing comment
-        comment_pos = s.find('--')
+        comment_pos = s.find("--")
         if comment_pos > -1:
             s = s[0:comment_pos].strip()
 
         # remove new line
         if len(s) > 0:
-            s_result = s_result + s + ' ' # add space instead of new line
+            s_result = s_result + s + " "  # add space instead of new line
 
     return s_result
 
-'''
+
+"""
 ----------------------------------------------------
     nice output about execution result
 
@@ -200,76 +208,86 @@ def troubleshooting_bqc_format(bqc):
     status:     0 = ok, !0 = error
     msg:        additional info if there is any
 ----------------------------------------------------
-'''
+"""
+
+
 def nice_message(s_filename, status, msg):
-    time =    datetime.datetime.now()
-    file =    s_filename.ljust(35, ' ')
-    result =  'Done.' if status==0 else 'Error'
-    message = '' if len(msg)==0 else ': ' + msg if len(msg.split('\n')) == 1 \
-        else '\n' + '\n'.join(map(lambda x: ''.ljust(4) + x, msg.split('\n')))
+    time = datetime.datetime.now()
+    file = s_filename.ljust(35, " ")
+    result = "Done." if status == 0 else "Error"
+    message = (
+        ""
+        if len(msg) == 0
+        else ": " + msg
+        if len(msg.split("\n")) == 1
+        else "\n" + "\n".join(map(lambda x: "".ljust(4) + x, msg.split("\n")))
+    )
 
-    return '{0} | {1} | {2}{3}'.format(time, file, result, message)
+    return "{0} | {1} | {2}{3}".format(time, file, result, message)
 
 
-'''
+"""
 ----------------------------------------------------
     main()
     return codes: 0 = ok, !0 = error
 ----------------------------------------------------
-'''
+"""
+
 
 def main():
-
     rc = 0
     duration = datetime.datetime.now()
     params = read_params()
-    config = read_config(params['etlconf_file'], params['config_file'])
+    config = read_config(params["etlconf_file"], params["config_file"])
 
     # stop if any files are not found
 
-    if len(params['files_not_found']) > 0:
-        rc = 2 # No such file or directory # Linux OS error code
-        for s_filename in params['files_not_found']:
-            print('No such file or directory: {file}\n'.format(file=s_filename))
+    if len(params["files_not_found"]) > 0:
+        rc = 2  # No such file or directory # Linux OS error code
+        for s_filename in params["files_not_found"]:
+            print("No such file or directory: {file}\n".format(file=s_filename))
 
     else:
-        bq_command = "bq query --use_legacy_sql=false \"{query}\""
+        bq_command = 'bq query --use_legacy_sql=false "{query}"'
         s_done = []
-        s_done.append(nice_message('start...', 0, ''))
+        s_done.append(nice_message("start...", 0, ""))
 
-        for s_filename in params['script_files']:
+        for s_filename in params["script_files"]:
+            print("Run script {file}\n".format(file=s_filename))
 
-            print('Run script {file}\n'.format(file=s_filename))
-
-            s_queries = open(s_filename).read().split(';')
+            s_queries = open(s_filename).read().split(";")
             s_queries = trim_queries(s_queries)
-            
+
             query_no = 0
             for s_query in s_queries:
-
                 bqc = bq_command.format(
                     query=troubleshooting_bqc_format(format_query(s_query, config))
                 )
                 query_no += 1
-                print('Starting query...')
+                print("Starting query...")
 
                 rc = os.system(bqc)
 
                 if rc != 0:
                     break
-            
+
             s_done.append(
-                nice_message(s_filename, rc, '' if rc==0 else 'See query No {0}'.format(query_no)))
+                nice_message(
+                    s_filename,
+                    rc,
+                    "" if rc == 0 else "See query No {0}".format(query_no),
+                )
+            )
 
             if rc != 0:
                 break
 
-        print('\nScripts executed:')
+        print("\nScripts executed:")
         for a in s_done:
             print(a)
         duration = datetime.datetime.now() - duration
-        print('Run time: {0}'.format(duration)) # timedelta HH:MM:SS.f
-        
+        print("Run time: {0}".format(duration))  # timedelta HH:MM:SS.f
+
     return rc
 
 
@@ -278,7 +296,7 @@ def main():
 # ----------------------------------------------------
 return_code = main()
 
-print('bq_run_script.exit()', return_code)
+print("bq_run_script.exit()", return_code)
 exit(return_code)
 
 # last edit: 2021-02-07
